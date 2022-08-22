ac30794a2a1d185fa7f0664d77ad2717b6e8d8f2
leozamboni <leonardonunes169@gmail.com>
2022-08-22 16:18:27 -0300

    print output in main

--- src/lpp.py
+++ src/lpp.py
@@ -1,10 +1,12 @@
 from transpiler.main import Transpiler
-
+from pathlib import Path
+txt = Path('./examples/example1.lpp').read_text()
 class LPP:
   transpiler = None
 
   def __init__(self):
-    self.transpiler = Transpiler('programa Exemplo \n')
+    self.transpiler = Transpiler(txt)
     self.transpiler.run()
+    print(self.transpiler.stdout)
 
 LPP()
