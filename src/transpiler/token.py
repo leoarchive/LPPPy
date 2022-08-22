92cc897519f35c67061a6c2677d126d1a84297b1
leozamboni <leonardonunes169@gmail.com>
2022-08-22 12:23:42 -0300

    more token types

--- src/transpiler/token.py
+++ src/transpiler/token.py
@@ -1,15 +1,17 @@
-from email.policy import default
 from enum import Enum
 
 class TokenTypes(Enum):
-  id      = 1
-  lArrow  = 2
-  rArrow  = 3
+  id        = 1
+  numb      = 2
+  lArrow    = 3
+  rArrow    = 4
+  bLine     = 5
+  programa  = 6
 
 class Token:
-  key   = ''
-  type  = 0
-  line  = 0
+  key       = ''
+  type      = 0
+  line      = 0
 
   def __init__(self, key, type, line):
     self.key  = key
@@ -18,6 +20,8 @@ class Token:
 
   def getType(key):
     match key:
+      case 'programa':
+        return TokenTypes.programa
       case '←':
         return TokenTypes.rArrow
       case '→':
