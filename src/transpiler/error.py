b77add50a3786ef3f407b39bea4cea75fa23b64b
leozamboni <leonardonunes169@gmail.com>
2022-08-22 16:18:53 -0300

    temp hot fix in error

--- src/transpiler/error.py
+++ src/transpiler/error.py
@@ -6,10 +6,13 @@ class ErrorTypes(Enum):
   parser_unexpected_token = 2
 
 class Error:
-  def __init__(self, type, key, line):
-    print(f"{self.getMsg(type)} '{key}' na linha {line}\n", file=sys.stderr)
+  def __init__(self, type, token):
+    if (not token): exit()
+    print(f"{self.getMsg(type)} '{token.key}' na linha {token.line}\n", file=sys.stderr)
 
   def getMsg(self, type):
     match type:
       case ErrorTypes.lexer_unexpected_token:
         return 'lexer: token inesperado'
+      case ErrorTypes.parser_unexpected_token:
+        return 'parser: token inesperado'
