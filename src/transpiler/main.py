9f616e335b0427ef973dad5cf36670255a43e7fd
leozamboni <leonardonunes169@gmail.com>
2022-08-22 12:24:22 -0300

    refact main transpiler file

--- src/transpiler/main.py
+++ src/transpiler/main.py
@@ -1,10 +1,18 @@
 from .lex import Lexer
+from .parser import Parser
 
 class Transpiler:
-  stdin = ''
+  stdin   = ''
+  lexer   = None 
+  parser  = None 
 
   def __init__(self, stdin):
-    self.stdin = stdin
-    self.lexer = Lexer(stdin)
-    print(self.lexer.lex().__dict__)
-    print(self.lexer.lex().__dict__)
+    self.stdin  = stdin
+    self.lexer  = Lexer(stdin)
+    self.parser = Parser(self.lexer)
+
+  def run(self):
+    self.parser.run()
+
+    # print(self.lexer.lex().__dict__)
+    # print(self.lexer.lex().__dict__)
